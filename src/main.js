2fe133ec26ea6ad4fc439d5d6d88ec44f2aefebd
evan <evan.hermier@gmail.com>
2021-12-18 22:42:08 +0100

    ajout highlight.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from "./router";
 import Markdown from "vue3-markdown-it";
 import "./assets/styles/style.css";
 import "./assets/styles/mobile-nav.css";
+import "highlight.js/styles/nord.css";
 
 createApp(App).use(router).use(Markdown).mount("#app");
