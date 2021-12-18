6fcee54b3ec3e4da8471dc1eec594d91d19152b4
evan <evan.hermier@gmail.com>
2021-12-18 17:46:05 +0100

    import markdown-it + test

--- src/components/Markdown/Markdown.vue
+++ src/components/Markdown/Markdown.vue
@@ -4,16 +4,16 @@
     <div id="content">
       <div id="editorContent">
         <h2>Editor</h2>
-        <textarea
-          id="editor"
-          rows="15"
-          cols="60"
-          v-model="translatedText"
-        ></textarea>
+        <textarea id="editor" rows="15" cols="60" v-model="source"></textarea>
       </div>
       <div id="preview" class="shadow">
-        <h2>Preview</h2>
-        <div v-html="convertText"></div>
+        <Markdown
+          :source="source"
+          :html="true"
+          :breaks="true"
+          :xhtmlOut="true"
+          :typographer="true"
+        />
       </div>
     </div>
     <div class="link-to-challenge">
@@ -28,8 +28,62 @@
 </template>
 
 <script>
+import Markdown from "vue3-markdown-it";
 export default {
   name: "markdown",
+  components: {
+    Markdown,
+  },
+  data() {
+    return {
+      source: "",
+    };
+  },
+  mounted() {
+    this.source = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+        function anotherExample(firstLine, lastLine) {
+          if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+            return multiLineCode;
+          }
+        }
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Markdown from "vue3-markdown-it";
 import "./assets/styles/style.css";
 import "./assets/styles/mobile-nav.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(Markdown).mount("#app");
